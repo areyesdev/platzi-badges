b22180b077c14791b80a524b2eca9e05275ac48b
Andres Reyes | Ubuntu <ar97yf@gmail.com>
2020-11-09 17:03:43 -0500

    api loader

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,47 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/BadgesList.css';
 
 class BadgesListItem extends React.Component {
-    render() {
-        return (
-            <div className="BadgesListItem">
-                <img
-                    className="BadgesListItem__avatar"
-                    src={this.props.badge.avatarUrl}
-                    alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
-                />
+  render() {
+    return (
+      <div className="BadgesListItem">
+        <img
+          className="BadgesListItem__avatar"
+          src={this.props.badge.avatarUrl}
+          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+        />
 
-                <div>
-                    <strong>
-                        {this.props.badge.firstName} {this.props.badge.lastName}
-                    </strong>
-                    <div className="Twitter__name">
-                        <span className="Twitter__logo"></span>@{this.props.badge.twitter}
-                    </div>
-                    {this.props.badge.jobTitle}
-                </div>
-            </div>
-        );
-    }
+        <div>
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong>
+          <div className="Twitter__name">
+            <span className="Twitter__logo"></span>@{this.props.badge.twitter}
+          </div>
+          {this.props.badge.jobTitle}
+        </div>
+      </div>
+    );
+  }
 }
 
 class BadgesList extends React.Component {
-    render() {
-        return (
-            <div className="BadgesList">
-                <ul className="list-unstyled">
-                    {this.props.badges.map(badge => {
-                        return (
-                            <li key={badge.id}>
-                                <BadgesListItem badge={badge} />
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
+  render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      );
     }
+    return (
+      <div className="BadgesList">
+        <ul className="list-unstyled">
+          {this.props.badges.map((badge) => {
+            return (
+              <li key={badge.id}>
+                <BadgesListItem badge={badge} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default BadgesList;
